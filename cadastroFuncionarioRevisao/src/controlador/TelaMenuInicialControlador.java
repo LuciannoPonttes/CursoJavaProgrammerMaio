69e1cb2ead812675da4687ea0473ef99d2090eb3
LuciannoPonttes <luciannoponttes@gmail.com>
2024-06-03 21:27:42 -0300

    Revisao 2

--- cadastroFuncionarioRevisao/src/controlador/TelaMenuInicialControlador.java
+++ cadastroFuncionarioRevisao/src/controlador/TelaMenuInicialControlador.java
@@ -4,11 +4,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.imageio.plugins.tiff.ExifGPSTagSet;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+
+import tela.TelaMenuGerente;
 
 public class TelaMenuInicialControlador implements ActionListener {
 
+	//Atributos da classe
 	JTextField opcao;
+	
+	//Telas
+	TelaMenuGerente telaMenuGerente = new TelaMenuGerente();
 	
 	
 	public TelaMenuInicialControlador(JTextField opcao) {
@@ -18,25 +25,30 @@ public class TelaMenuInicialControlador implements ActionListener {
 	@Override//Vamos entrar no polimorfismo
 	public void actionPerformed(ActionEvent e) {
 		
-		
-			switch (opcao.getText()) {
-				case "1": {
-					System.out.println("Direcione para tela do gerente");
-					break;
-					
-				}
-				case "2": {
-					
-					System.out.println("Direcione para tela do atendente");
-					break;
-				}
-				case "3": {
-						System.exit(0);
+		if(opcao.getText().equals("1") || opcao.getText().equals("2") ) {
+				switch (opcao.getText()) {
+					case "1": {
+						telaMenuGerente.chamarTelaMenuGerente();
+						System.out.println("Direcione para tela do gerente");
 						break;
 						
+					}
+					case "2": {
+						
+						System.out.println("Direcione para tela do atendente");
+						break;
+					}
+					case "3": {
+							System.exit(0);
+							break;
+							
+					}
+					
 				}
+		}else {
+			JOptionPane.showMessageDialog(null, "A opcão " + opcao.getText() +" Não é valida");
 			
-			}
+		}
 		
 	}
 
