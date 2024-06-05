246d4d6eb43e7cd8f6d98d0d12396189bf00bbf8
LuciannoPonttes <luciannoponttes@gmail.com>
2024-06-05 20:01:44 -0300

    banco de dados

--- bancoDeDados/src/bancoDeDados/Principal.java
+++ bancoDeDados/src/bancoDeDados/Principal.java
@@ -1,12 +1,21 @@
 package bancoDeDados;
 
+import java.sql.Connection;
+
 public class Principal {
 	
 	public static void main (String args[]) {
 		
-		FabricaConexao conexao = new FabricaConexao();
+		FabricaConexao conexao = new FabricaConexao();// Instancia a classe que contém o metodo que retorna o objeto de conexão
 		
-		conexao.criarConexaoComBaseExemplo();
+
+		Connection conexãoRecebida = conexao.criarConexaoComBaseExemplo();
+		
+		if (conexãoRecebida == null) {
+			System.out.println("Deu ruim a conexão e não posso usar o objeto");
+		}else {
+			System.out.println("Deu bom e posso usar a conexão");
+		}
 		
 	}
 
